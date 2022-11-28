a624188c018b25f87e58805097e80fa99fd47691
Frank Wacker <92989923+wackerfra@users.noreply.github.com>
2022-11-28 19:52:50 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='HabitTrackerIU',
     extras_require=dict(tests=['pytest']),
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
+    version="0.1.0",
+    py_modules=["main"],
+    install_requires=["click", "tabulate"],
+    entry_points={"console_scripts": ["habit = main:main_cli"]}
 
 )
