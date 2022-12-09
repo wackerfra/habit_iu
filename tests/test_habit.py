9e198ecc7c7d678ab420e2e1bfcc77863cbc027f
FrankWacker <wackefra@outlook.com>
2022-12-09 16:01:50 +0100

    2.12.22-Office

--- tests/test_habit.py
+++ tests/test_habit.py
@@ -1,2 +1,22 @@
-def test_habit():
-    assert True
+import habit
+import pytest
+
+def test_connect_db():
+    assert habit.connect_db() is not None
+
+
+def test_get_instance():
+    habit1 = habit.Habit.get_instance(2)
+    assert habit1.name == 'Tea'
+    assert habit1.description == 'drink more tea'
+
+
+# def test_habit():
+#     # Arrange
+#     #Act
+#     #Assert
+#
+#     # calculator = Calculator()
+#     # result = calculator.add(2, 3)
+#     # assert result == 5
+#     assert True
